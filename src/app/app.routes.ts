64b316dff259090d337924d2a9e6e9e47681ee0b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-04-25 14:28:48 -0300

    feat(routes): adiciona rotas iniciais para redirecionamento e carregamento da biblioteca 'home'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('@pontuei/home').then((m) => m.homeRoutes),
+  },
+];
