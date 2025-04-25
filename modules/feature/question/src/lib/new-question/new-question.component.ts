9c22cb6a9ddfeab6fdf1680b1c89bb716a05f627
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-04-25 16:55:50 -0300

    feat(new-question): adiciona campos para nome do contador, disciplina e observação, além de implementar a funcionalidade de salvar contador

--- modules/feature/question/src/lib/new-question/new-question.component.ts
+++ modules/feature/question/src/lib/new-question/new-question.component.ts
@@ -1,16 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-new-question',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './new-question.component.html',
   styleUrls: ['./new-question.component.scss'],
 })
 export class NewQuestionComponent {
   acertos = 0;
   erros = 0;
+  nomeContador = '';
+  disciplinaSelecionada = '';
+  observacao = '';
+  disciplinas = [
+    'Matemática',
+    'Português',
+    'História',
+    'Geografia',
+    'Biologia',
+    'Física',
+    'Química',
+    'Inglês',
+    'Outra',
+  ];
 
   incrementarAcertos() {
     this.acertos++;
@@ -18,5 +33,16 @@ export class NewQuestionComponent {
 
   incrementarErros() {
     this.erros++;
+  }
+
+  salvarContador() {
+    // Aqui você pode implementar a lógica para salvar o contador
+    console.log({
+      nome: this.nomeContador,
+      disciplina: this.disciplinaSelecionada,
+      observacao: this.observacao,
+      acertos: this.acertos,
+      erros: this.erros,
+    });
   }
 }
