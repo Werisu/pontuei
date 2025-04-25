37ccf2a127b1d5a0fbf1068bb4513e623e6b6187
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-04-25 14:55:34 -0300

    feat(exercises-ui): cria nova biblioteca 'exercises-ui' com configuração de ESLint, Jest e TypeScript, além de componentes e README.md

---
+++ modules/ui/exercises/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/exercises-ui/exercises-ui.component';
+
+export * from './lib/components/exercises-card/exercises-card.component';
---
+++ modules/ui/exercises/src/lib/components/exercises-card/exercises-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-exercises-card',
+  imports: [CommonModule],
+  templateUrl: './exercises-card.component.html',
+  styleUrl: './exercises-card.component.scss',
+})
+export class ExercisesCardComponent {}
