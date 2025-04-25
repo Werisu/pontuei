98570d8df14ccd069cfb983c8ff42794713879c5
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-04-25 15:03:09 -0300

    feat(home): substitui conteúdo do componente Home por 'exercises-card' e atualiza importações

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ExercisesCardComponent } from '@pontuei/exercises-ui';
 
 @Component({
   selector: 'lib-home',
-  imports: [CommonModule],
+  imports: [CommonModule, ExercisesCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- modules/ui/exercises/src/index.ts
+++ modules/ui/exercises/src/index.ts
@@ -1,3 +1,1 @@
-export * from './lib/exercises-ui/exercises-ui.component';
-
 export * from './lib/components/exercises-card/exercises-card.component';
